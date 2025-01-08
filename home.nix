31ed0349e75abd1d86a2343d7192ada21fb43b93
W. S. Wellington <thewellington@gmail.com>
2025-01-07 22:11:41 -0800

    trying to work with .vimrc

--- home.nix
+++ home.nix
@@ -1,16 +1,21 @@
 users.users.thewellington.isNormalUser = true;
-home-manager.users.thewellington = { pkgs, ... }: {
+home-manager.users.thewellington = { config, pkgs, ... }: {
   home.packages = [ pkgs.atool pkgs.httpie ];
+
   programs.bash.enable = true;
-  programs.vim= {
+
+  programs.vim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [ vim-airline ];
-    settings = { ignorecase = true;
-		numbers = true;
+    settings = { 
+	ignorecase = true;
+	number = true;
+	tabstop = 2;
     };
     extraConfig = ''
 	set mouse=a
     '';
+
   };
 
   # The state version is required and should stay at the version you
