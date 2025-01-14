58a3a2f2284334d1cbce9b08048e6382cf38dc7b
W. S. Wellington <thewellington@gmail.com>
2025-01-13 21:52:17 -0800

    woot!  we are managing vim, and other apps through home-managergit add .!

--- home.nix
+++ home.nix
@@ -3,6 +3,43 @@
   home.homeDirectory = "/home/thewellington";
   programs.home-manager.enable = true;
 
+  home.packages = with pkgs; [
+  
+  # misc
+  cowsay
+
+  #utils
+  gnutar
+  zip
+  unzip
+  p7zip
+  
+  # networking
+  ipcalc
+  dnsutils
+  ldns
+  # nmap
+  
+  # productivity
+  glow
+  #vim # currently installed by system config
+  
+  # coding
+  #git
+
+  ];
+
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [ vim-airline ];
+    settings = {
+      ignorecase = true;
+      number = true;
+    };
+    extraConfig = ''
+      set mouse=a
+    '';
+  };
 
   # The state version is required and should stay at the version you
   # originally installed.
