0cb3abf49837e32ff2ef0378aa084155a1c81f7e
W. S. Wellington <thewellington@gmail.com>
2025-01-13 21:34:27 -0800

    is home-manager working for once?

--- flake.nix
+++ flake.nix
@@ -3,39 +3,40 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";  
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";  
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {self, nixpkgs, home-manager, ...  }:
+  outputs = {self, nixpkgs, home-manager, ...  }: 
     let 
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-    nixosConfigurations= {
+    nixosConfigurations = {
       pinkie-pie = lib.nixosSystem {
         inherit system;
-        modules = 
-	  [ 
-	    ./configuration.nix
-
-	    ./modules/coding.nix
-	    ./modules/flipper.nix
-            ./modules/internet-packages.nix
-	    ./modules/network-packages.nix
-	    ./modules/radio.nix
-            ./modules/security.nix
-	  ];
+        modules = [ 
+	  ./configuration.nix
+	  ./modules/coding.nix
+	  ./modules/flipper.nix
+          ./modules/internet-packages.nix
+	  ./modules/network-packages.nix
+	  ./modules/radio.nix
+          ./modules/security.nix
+	];
       };
     };
+
+    defaultPackage.${system} = home-manager.defaultPackage.${system};
+
     homeConfigurations = {
       thewellington = home-manager.lib.homeManagerConfiguration {
-	inherit pkgs;
-
-	modules = [ ./home.nix ];
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
-
 }
--- home.nix
+++ home.nix
@@ -1,26 +1,11 @@
-users.users.thewellington.isNormalUser = true;
-home-manager.users.thewellington = { config, pkgs, ... }: {
-  home.packages = [ pkgs.atool pkgs.httpie ];
+{ config, pkgs, ... }: {
+  home.username = "thewellington";
+  home.homeDirectory = "/home/thewellington";
+  programs.home-manager.enable = true;
 
-  programs.bash.enable = true;
-
-  programs.vim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [ vim-airline ];
-    settings = { 
-	ignorecase = true;
-	number = true;
-	tabstop = 2;
-    };
-    extraConfig = ''
-	set mouse=a
-    '';
-    };
-
-  };
 
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "24.05";
-};
+}
 
