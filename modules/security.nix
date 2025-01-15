cd4343a904a21bafdef83a8668f2dac66067a26e
W. S. Wellington <thewellington@gmail.com>
2025-01-14 17:48:52 -0800

    pulling all nord refs for now...

--- modules/security.nix
+++ modules/security.nix
@@ -7,6 +7,21 @@
     _1password-cli
     _1password-gui
     gnupg
+
+    # vpn
+    #wgnord
+    #gnomeExtensions.nordvpn-quick-toggle
+
+    # yubikey
+    yubikey-manager
+    yubikey-manager-qt
+    yubikey-agent
+    yubikey-touch-detector
+    yubikey-personalization
+    yubikey-personalization-gui
+    piv-agent
   ];
+
+
 }
 
