fba001b430a258be7d6d6ad4ac552a17e6355d5a
W. S. Wellington <thewellington@gmail.com>
2026-01-28 12:26:52 -0800

    adding zed to rainbow dash

--- modules/coding.nix
+++ modules/coding.nix
@@ -1,10 +1,11 @@
+
 #### Installs coding tools
 
 { config, lib, pkgs, ... }: {
   home.packages = with pkgs; [
     # tools
     direnv
-    
+
     #languages
     python313
     python313Packages.pip
@@ -19,13 +20,29 @@
     };
     extraConfig = ''
      set mouse-=a
-    ''; 
+    '';
   };
+
+   programs.zed-editor = {
+     enable = true;
+     extensions = [ "nix" "toml" "rust" "make" "python" "bash" ];
+     userSettings = {
+       theme = {
+         mode = "system";
+         dark = "One Dark";
+         light = "One Light";
+       };
+       hour_format = "hour24";
+       vim_mode = true;
+     };
+   };
 
   programs.git = {
     enable = true;
     settings.user.name = "W. S. Wellington";
     settings.user.email = "thewellington@gmail.com";
   };
+
+
 
 }
