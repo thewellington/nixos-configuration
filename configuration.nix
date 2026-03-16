a52c57c154a30fbd968001416fce1ef27ab60e54
W. S. Wellington <thewellington@gmail.com>
2026-03-15 20:13:23 -0700

    adding dock

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,7 @@
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # gnomeExtensions.topicons-plus
     gnomeExtensions.appindicator
+    gnomeExtensions.dash-to-dock
   ];
 
 
